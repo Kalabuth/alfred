e98dab2b6f8b0e76eb9dc1947f9910a5b80140f2
Kalabuth <youtube_.1034@hotmail.com>
2025-04-29 00:27:44 -0500

    feat(common): Add custom pagination with page not found handling

---
+++ apps/common/methods/custom_pagination.py
@@ -0,0 +1,29 @@
+from rest_framework.exceptions import NotFound
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+
+
+class CustomPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = "page_size"
+    max_page_size = 100
+
+    def paginate_queryset(self, queryset, request, view=None):
+        try:
+            return super().paginate_queryset(queryset, request, view)
+        except NotFound:
+            request.query_params._mutable = True
+            request.query_params["page"] = "last"
+            return super().paginate_queryset(queryset, request, view)
+
+    def get_paginated_response(self, data):
+        return Response(
+            {
+                "next": self.get_next_link(),
+                "previous": self.get_previous_link(),
+                "count": self.page.paginator.count,
+                "total_pages": self.page.paginator.num_pages,
+                "current_page": self.page.number,
+                "results": data,
+            }
+        )
